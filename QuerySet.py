c0d482a1bb5ebe74841e8dbc3359be77b4999f8a
Thanseeh yaseen <yaseenkdr27@gmail.com>
2023-06-22 10:55:19 +0530

    filter querying

--- QuerySet.py
+++ QuerySet.py
@@ -23,3 +23,27 @@ student2 = Student.objects.create(
 #Query all Student details
 students = Student.objects.all()
 print(students)
+
+#Retrieving Single Objects from QuerySets
+queryset = Student.objects.get(pk = 1)  
+print(queryset)
+
+#Filtering the Records
+queryset = Student.objects.filter(username__startswith = 'a')
+print(queryset)
+
+# filetr options :
+# 1. startswith:
+#    Syntax: field__startswith=value
+#    Example: objects.filter(title__startswith='A')
+#    Retrieves objects where the specified field starts with the given value.
+
+# 2. endswith:
+#    Syntax: field__endswith=value
+#    Example: objects.filter(title__endswith='Z')
+#    Retrieves objects where the specified field ends with the given value.
+
+# 3. contains:
+#    Syntax: field__contains=value
+#    Example: objects.filter(title__contains='book')
+#    Retrieves objects where the specified field contains the given value.
