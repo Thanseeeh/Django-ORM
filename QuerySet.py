8c40198f91616d165419cbbda279f344669af814
Thanseeh yaseen <yaseenkdr27@gmail.com>
2023-06-22 12:08:59 +0530

    Q object and bulk_create method

--- QuerySet.py
+++ QuerySet.py
@@ -47,3 +47,28 @@ print(queryset)
 #    Syntax: field__contains=value
 #    Example: objects.filter(title__contains='book')
 #    Retrieves objects where the specified field contains the given value.
+
+#Exclude() method
+queryset = Student.objects.exclude(username__startswith = 'a')
+print(queryset)
+
+#OR and AND operations
+#OR:
+queryset = Student.objects.filter(first_name__startswith = 'R') | Student.objects.filter(last_name__startswith = 'S')
+print(queryset)
+#AND:
+queryset = Student.objects.filter(first_name__startswith = 'P') & Student.objects.filter(last_name__startswith = 'S')
+print(queryset)
+
+#Q object
+from django.db.models import Q
+queryset3 = Student.objects.filter(Q(first_name__startswith='R') & Q(last_name__startswith='D') ) #Q object AND operation
+queryset3 = Student.objects.filter(Q(first_name__startswith='R') | Q(last_name__startswith='D') ) #Q object OR operation
+
+#Bulk_Create() method
+#sometimes we want to create multiple objects in one shot. django ORM provides the bulk_create to create multiple objects in one way.
+Student.objects.bulk_create([
+    Student(first_name = 'Jai', last_name = 'Shah', mobile = '88888', email = 'shah@reddif.com'),
+    Student(first_name = 'Tarak', last_name = 'Mehta', mobile = '9999', email = 'tarak@reddif.com'),
+    Student(first_name = 'SuryaKumar', last_name = 'Yadav', mobile = '00000', email = 'yadav@reddif.com')
+    ])
